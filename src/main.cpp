3286ee4e8a71ded3bae567796694f0d920b9694b
Aquila <aquila@eri.av>
2016-07-21 08:21:21 +0300

    Добавлена демонизация сервера.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 //https://bitbucket.org/snippets/vlasovmaksim/krMpr#file-final.md
 
 #include <iostream>
+#include <unistd.h>
+#include <stdio.h>
 #include "commandparameters.h"
 #include "httpserver.h"
 
@@ -10,11 +12,17 @@ int main(int argc, char **argv) {
 	CommandParameters server_options(argc, argv);
 	cout << "Host address: " << server_options.getHostAddr() << endl << "Port: " << server_options.getPort() << endl << "File directory: " << server_options.getDirectory() << endl;
 	HttpServer server(server_options.getHostAddr(), server_options.getPort(), server_options.getDirectory());
-	bool res = server.run();
-	if (!res) {
-		cout << "Server failed: " << server.getLastError() << endl;
-		return -1;
-	}
-	cout << "Server successufully complete" << endl;
+	int daemon_res = daemon(0, 1);
+	if (daemon_res == 0) {
+		//child
+		//fclose(stdout);
+		//fclose(stdin);
+		//fclose(stderr);
+		bool res = server.run();
+		if (!res) {
+			cout << "Server failed: " << server.getLastError() << endl;
+			return -1;
+		}
+	}	
 	return 0;
 }
